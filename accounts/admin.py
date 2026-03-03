1074ba410d4f0a18e1fe5d582d21a338923ac84f
ev3rGr <mouadlahrizi2@gmail.com>
2026-03-03 10:30:07 +0000

    Adding Custom user and configuring it

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
+from django.contrib import admin  
+from django.contrib.auth.admin import UserAdmin  
+from .models import User  
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom Fields", {"fields": ("token",)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Custom Fields", {"fields": ("token",)}),
+    )
+
+    list_display = ("username", "email", "token", "is_staff")
+
+admin.site.register(User, CustomUserAdmin)
