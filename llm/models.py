0c61ec5e4398c0f72d51b1492bd079cef0acf7cd
ev3rGr <mouadlahrizi2@gmail.com>
2026-03-03 10:46:15 +0000

    Adding llm model

--- llm/models.py
+++ llm/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class LLMModel(models.Model):
+
+    model_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.model_name
